2347d2e05e48a4e8449d912b5361bc5347c991a8
Jonathan Royle <jono.royle@gmail.com>
2022-11-18 18:45:13 +0100

    Add wall collisions as reflections

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,12 +7,15 @@ public class BallController : MonoBehaviour
 {
     public GameObject BallTemplate;
     public Camera PlayerCam;
-    public float BallLaunchSpeed = 40f;
-    public float Gravity = 9.8f;
-    public float BallDiameter = 0.06f;
-    public float DragCoefficient = 0.5f;
-    public float AirDenisty = 1.2f;
-    public float BallMass = 0.06f;
+    public List<BoxCollider> Walls;
+    public BoxCollider Ground;
+    [Range(1f, 100)] public float BallLaunchSpeed = 40f;
+    [Range(0.1f, 25)] public float Gravity = 9.8f;
+    [Range(0.01f, 2)] public float BallDiameter = 0.06f;
+    [Range(0, 2)] public float DragCoefficient = 0.5f;
+    [Range(0, 10)] public float AirDenisty = 1.2f;
+    [Range(0.01f, 100)] public float BallMass = 0.06f;
+    [Range(0, 1)] public float RestitutionCoefficient = 0.7f;
 
     private Dictionary<string, Ball> _ballCollection = new Dictionary<string, Ball>();
     private int _ballCount = 0;
@@ -20,13 +23,15 @@ public class BallController : MonoBehaviour
     private Vector3 _gravityVector;
     private Vector3 _scaleVector;
     private float _ballCrossSectionalArea;
+    private float _ballRadius;
 
     // Start is called before the first frame update
     void Start()
     {
         _gravityVector = new Vector3(0, -1 * Gravity, 0);
+        _ballRadius = BallDiameter / 2;
         _scaleVector = new Vector3(BallDiameter, BallDiameter, BallDiameter);
-        _ballCrossSectionalArea = Mathf.PI * Mathf.Pow((BallDiameter / 2), 2);
+        _ballCrossSectionalArea = Mathf.PI * Mathf.Pow(_ballRadius, 2);
     }
 
     // Update is called once per frame
@@ -44,12 +49,49 @@ public class BallController : MonoBehaviour
 
         foreach (var namedBall in _ballCollection)
         {
-            ApplyGravity(namedBall.Value);
+            Vector3 newBallPosition;
+            Vector3 currentBallPosition = namedBall.Value.BallObject.transform.position;
+
+            Vector3 closestPointOnGround = Ground.bounds.ClosestPoint(currentBallPosition);
+            float distanceToGround = Vector3.Distance(currentBallPosition, closestPointOnGround);
+            if (distanceToGround <= _ballRadius)
+            {
+                currentBallPosition = currentBallPosition + Vector3.up * (_ballRadius - distanceToGround);
+            }
+            else
+            {
+                ApplyGravity(namedBall.Value);
+            }
             ApplyDrag(namedBall.Value);
-            //Movement
-            namedBall.Value.BallObject.transform.position += (namedBall.Value.Velocity * Time.deltaTime);
-            //TODO Ball spin
+            if (namedBall.Value.Velocity.magnitude > 0.2)
+            {
+                newBallPosition = currentBallPosition + (namedBall.Value.Velocity * Time.deltaTime);
+            }
+            else
+            {
+                newBallPosition = currentBallPosition;
+            }
+
+            foreach (var wall in Walls)
+            {
+                newBallPosition = PreventWallClipping(newBallPosition, wall);
+            }
+
+            namedBall.Value.BallObject.transform.position = newBallPosition;
+                //TODO Ball spin
         }
+    }
+
+    private Vector3 PreventWallClipping(Vector3 newBallPosition, BoxCollider wall)
+    {
+        Vector3 closestPointOnWall = wall.bounds.ClosestPoint(newBallPosition);
+        float distanceToWall = Vector3.Distance(newBallPosition, closestPointOnWall);
+        if (distanceToWall <= _ballRadius)
+        {
+            newBallPosition += (newBallPosition - closestPointOnWall);
+        }
+
+        return newBallPosition;
     }
 
     private void ApplyGravity(Ball ball)
@@ -85,6 +127,13 @@ public class BallController : MonoBehaviour
 
     public void OnBallCollision(BallCollisionContainer collision)
     {
-        //TODO collision calculation
+        //TODO ball-ball collision
+        Ball colliderBall = _ballCollection[collision.Ball.name];
+        float ballSpeed = colliderBall.Velocity.magnitude;
+        Vector3 intialDirection = colliderBall.Velocity.normalized;
+        Vector3 collisionNormal = collision.Collision.contacts[0].normal;
+        //Reflection of a vector V against a plane with normal N: R=V-2N(VdotN)
+        Vector3 newDirection = intialDirection - 2 * Vector3.Dot(intialDirection, collisionNormal) * collisionNormal;
+        colliderBall.Velocity = newDirection.normalized * ballSpeed * RestitutionCoefficient;
     }
 }
